cd769af49250ad98f1bdcc05d2d10b857652cd8a
Shahryar Farokhi <shahriyarfarokhi.persia1374@gmail.com>
2022-07-13 17:30:24 +0430

    Lots of styles in SignUp component fixed

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -60,35 +60,66 @@ const SignUp = () => {
                 <h2 className={styles.header}>Sign Up</h2>
                 <div className={styles.formField}>
                     <label>Name</label>
-                    <input type="text" name='name' value={data.name} onChange={changeHandler} onFocus={focusHandler} />
+                    <input type="text"
+                        name='name'
+                        value={data.name}
+                        onChange={changeHandler}
+                        onFocus={focusHandler}
+                        className={errors.name && touch.name ? styles.uncompleted : styles.formInput}
+                    />
                     {errors.name && touch.name && <span>{errors.name}</span>}
                 </div>
 
                 <div className={styles.formField}>
                     <label>Email</label>
-                    <input type="email" name='email' value={data.email} onChange={changeHandler} onFocus={focusHandler} />
+                    <input type="email"
+                        name='email'
+                        value={data.email}
+                        onChange={changeHandler}
+                        onFocus={focusHandler}
+                        className={errors.email && touch.email ? styles.uncompleted : styles.formInput}
+                    />
                     {errors.email && touch.email && <span>{errors.email}</span>}
                 </div>
 
                 <div className={styles.formField}>
                     <label>Password</label>
-                    <input type="password" name='password' value={data.password} onChange={changeHandler} onFocus={focusHandler} />
+                    <input type="password"
+                        name='password'
+                        value={data.password}
+                        onChange={changeHandler}
+                        onFocus={focusHandler}
+                        className={errors.password && touch.password ? styles.uncompleted : styles.formInput}
+                    />
                     {errors.password && touch.password && <span>{errors.password}</span>}
                 </div>
 
                 <div className={styles.formField}>
                     <label>Confirm Password</label>
-                    <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler} />
+                    <input type="password"
+                        name='confirmPassword'
+                        value={data.confirmPassword}
+                        onChange={changeHandler}
+                        onFocus={focusHandler}
+                        className={errors.confirmPassword && touch.confirmPassword ? styles.uncompleted : styles.formInput}
+                    />
                     {errors.confirmPassword && touch.confirmPassword && <span>{errors.confirmPassword}</span>}
                 </div>
 
                 <div className={styles.formField}>
-                    <label>I accept terms of privacy policy</label>
-                    <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler} />
+                    <div className={styles.checkBoxContainer}>
+                        <label>I accept terms of privacy policy</label>
+                        <input type="checkbox"
+                            name='isAccepted'
+                            value={data.isAccepted}
+                            onChange={changeHandler}
+                            onFocus={focusHandler}
+                        />
+                    </div>
                     {errors.isAccepted && touch.isAccepted && <span>{errors.isAccepted}</span>}
                 </div>
 
-                <div>
+                <div className={styles.formButtons}>
                     <a href="#">Login</a>
                     <button type='submit'>Sign Up</button>
                 </div>
